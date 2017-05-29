77a72773b03840cd77b7166452127a16452b70e8
Arne Martin Aurlien <am@aurlien.net>
2017-05-29 22:49:56 +0200

    remove these

--- testit.js
+++
@@ -1,9 +0,0 @@
-var parser = require('./ansi-markdown');
-var fs = require('fs');
-
-var rawSlides = fs.readFileSync(process.argv[2], {encoding: 'UTF-8'});
-
-parser.render(rawSlides, function(data) {
-    console.log(data);
-});
-
